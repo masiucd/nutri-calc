15dfb20eb450f1782e234bcacb542cbf6250079a
Masiu <ciszekmarcell@gmail.com>
2019-12-09 18:46:47 +0100

    code code

---
+++ easy/deleteDuplicates.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const deleteDuplicates = function(head) {
+  //
+};
+
+// Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+// Example 1:
+
+// Input: 1->1->2
+// Output: 1->2
+// Example 2:
+
+// Input: 1->1->2->3->3
+// Output: 1->2->3
