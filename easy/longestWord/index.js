14eb651daa6fb22770086cfe6b59da04865b8799
Marcell Ciszek <you@example.com>
2020-01-08 10:08:53 +0100

    sorted

---
+++ easy/longestWord/index.js
@@ -0,0 +1,32 @@
+/* eslint-disable prefer-const */
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+const longestWord = function(words) {
+  let mySet = new Set();
+  let sortedWords = words.sort();
+  let longest = '';
+  for (let w of sortedWords) {
+    if (w.length === 1 || mySet.has(w.slice(0, -1))) {
+      mySet.add(w);
+      if (longest.length < w.length) {
+        longest = w;
+      }
+    }
+  }
+  return longest;
+};
+
+// const x1 = ['w', 'wo', 'wor', 'worl', 'world']; // world
+const x2 = ['a', 'banana', 'app', 'appl', 'ap', 'apply', 'apple']; // apple
+// console.log(longestWord(x1));
+console.log(longestWord(x2));
+
+// The word "world" can be built one character at a time by "w", "wo", "wor", and "worl".
+
+// Input:
+// words = ["a", "banana", "app", "appl", "ap", "apply", "apple"]
+// Output: "apple"
+// Explanation:
+// Both "apply" and "apple" can be built from other words in the dictionary. However, "apple" is lexicographically smaller than "apply".
