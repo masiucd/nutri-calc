f43b43aee1afb07f0237d3ec09cb7f5e5c597cc7
Masiu <ciszekmarcell@gmail.com>
2020-01-16 14:25:52 +0100

    add

--- easy/shortestToChar/index.js
+++ easy/shortestToChar/index.js
@@ -5,37 +5,48 @@
  * @param {string} S
  * @param {string} C
  */
-let getDistance = function(i, S, C) {
+
+const x = (i, S, C) => {
+  // let index = 0;
+  // while (i + index < S.length || i - index > -1) {
+  //   if (S[i + index] === C || S[i - index] === C) {
+  //     return index;
+  //   }
+  //   index += 1;
+  // }
+  // return index;
+
   let index = 0;
   while (i + index < S.length || i - index > -1) {
     if (S[i + index] === C || S[i - index] === C) {
       return index;
     }
-
     index += 1;
   }
-
   return index;
 };
+
 /**
  * @param {string} S
  * @param {character} C
  * @return {number[]}
  */
 const shortestToChar = function(S, C) {
+  let target = S.indexOf(C);
   const strArr = S.split('');
   const res = [];
+  // console.log(target);
   for (let i = 0; i < strArr.length; i++) {
-    let x = getDistance(i, S, C);
-
-    res.push(x);
+    let xs = x(i, S, C);
+    res.push(xs);
   }
 
-  console.log(res);
+  return res;
 };
 
 console.log(shortestToChar('loveleetcode', 'e'));
 // Output: [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
+console.log(shortestToChar('leo', 'o'));
 
 // Given a string S and a character C, return an array of integers representing the shortest distance from the character C in the string.
 
