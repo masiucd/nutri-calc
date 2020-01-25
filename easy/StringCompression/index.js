dc3149e74bdb1a32520fed2f68fcb9044e83abba
Masiu <ciszekmarcell@gmail.com>
2020-01-25 13:22:47 +0100

    ad

---
+++ easy/StringCompression/index.js
@@ -0,0 +1,41 @@
+/* eslint-disable prefer-const */
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+const compress = function(chars) {
+  let charMap = {};
+
+  for (let char of chars) {
+    charMap[char] = (charMap[char] || 0) + 1;
+  }
+
+  let charMapXs = [].concat(...Object.entries(charMap));
+
+  return charMapXs.map(String);
+};
+
+console.log(compress(['a', 'a', 'b', 'b', 'c', 'c', 'c']));
+// ["a","2","b","2","c","3"]
+
+// Given an array of characters, compress it in-place.
+
+// The length after compression must always be smaller than or equal to the original array.
+
+// Every element of the array should be a character (not int) of length 1.
+
+// After you are done modifying the input array in-place, return the new length of the array.
+
+// Follow up:
+// Could you solve it using only O(1) extra space?
+
+// Example 1:
+
+// Input:
+// ["a","a","b","b","c","c","c"]
+
+// Output:
+// Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+
+// Explanation:
+// "aa" is replaced by "a2". "bb" is replaced by "b2". "ccc" is replaced by "c3".
