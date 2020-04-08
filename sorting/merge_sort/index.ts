8280503161dc9f71cde1eb8f8b62c226d08ae81a
Masiu <ciszekmarcell@gmail.com>
2020-04-08 10:00:29 +0200

    merge sort

--- sorting/merge_sort/index.ts
+++ sorting/merge_sort/index.ts
@@ -1,16 +1,27 @@
-const merge = (left: number[], right: number) => {
-  //
+const merge = (left: number[], right: number[]) => {
+  let leftIndex = 0;
+  let rightIndex = 0;
+  const result = [];
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex += 1;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex += 1;
+    }
+  }
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 };
 
 const mergeSort = (numbers: number[]): number[] => {
   if (numbers.length <= 1) return numbers;
   const middle = Math.floor(numbers.length / 2);
-  const first = numbers.slice(0, middle);
+  const left = numbers.slice(0, middle);
   const right = numbers.slice(middle);
+  // console.log(left, right);
 
-  console.log(first, right);
-  return merge(mergeSort(first), mergeSort(right));
-  // return [2];
+  return merge(mergeSort(left), mergeSort(right));
 };
 
 console.log(mergeSort([6, 8, 100, 2, -11, 200, 5, 600, -2, 0, 1]));
