51c936f7416b25cf9df7cf170e94c94182d12974
Masiu <ciszekmarcell@gmail.com>
2020-08-09 19:19:47 +0200

    set up apollo server

--- README.md
+++ README.md
@@ -105,7 +105,7 @@ Add additional notes about how to deploy this on a live system.
 
 ## ✍️ Authors <a name = "authors"></a>
 
-* [@kylelobo](https://github.com/kylelobo) - Idea & Initial work
+* [@masiuciszek](https://github.com/masiuciszek) - Idea & Initial work
 
 See also the list of [contributors](https://github.com/kylelobo/The-Documentation-Compendium/contributors) who participated in this project.
 
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,35 @@
-console.log("hello from graphql");
+import { ApolloServer, gql } from "apollo-server";
+
+const dogsData = [
+  {
+    name: "Harry Potter",
+    owner: "J.K. Rowling",
+  },
+  {
+    name: "Jurassic Park",
+    owner: "Michael Crichton",
+  },
+];
+
+const typeDefs = gql`
+  type Dog {
+    name: String
+    owner: String
+  }
+
+  type Query {
+    dogs: [Dog]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    dogs: () => dogsData,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
