697a73ed8678d4b78aed3db61acd38462541e482
Masiu <ciszekmarcell@gmail.com>
2020-08-12 15:38:15 +0200

    mutations in graphql

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import { ApolloServer, gql } from "apollo-server";
+import { GraphQLScalarType, Kind } from "graphql";
+import connectDb from "./db/connectDb";
+import { Fighter as FighterType } from "./types";
 
 const fightersData = [
   {
@@ -8,6 +11,7 @@ const fightersData = [
     skills: ["Left kick", "Left side", "strong"],
     age: 36,
     status: "AGGRESSIVE",
+    createdAt: new Date("04-12-2020"),
   },
   {
     id: "212ssa@@33",
@@ -16,12 +20,15 @@ const fightersData = [
     skills: ["Smart", "Knees"],
     age: 25,
     status: "SMART",
+    createdAt: new Date("10-12-2002"),
   },
 ];
 
-type Fighter = typeof fightersData;
+// type Fighter = typeof fightersData;
 
 const typeDefs = gql`
+  scalar Date
+
   enum Status {
     VERY_AGGRESSIVE
     AGGRESSIVE
@@ -30,9 +37,14 @@ const typeDefs = gql`
     SMART
   }
 
-  # input FighterInput {
-
-  # }
+  input FighterInput {
+    id: ID
+    name: String
+    gym: String
+    skills: [String]
+    age: Int
+    createdAt: Date
+  }
 
   type Fighter {
     id: ID
@@ -41,18 +53,24 @@ const typeDefs = gql`
     skills: [String]!
     age: Int
     status: Status
+    createdAt: Date
   }
 
   type Query {
     fighters: [Fighter]
     fighter(id: ID): Fighter
   }
+
+  type Mutation {
+    addFighter(fighter: FighterInput): [Fighter]
+  }
 `;
 
 const resolvers = {
   Query: {
     fighters: () => fightersData,
-    fighter: (obj: any, args: { id: string }, context: any, info: any) => {
+    fighter: (parent: any, args: { id: string }, context: any, info: any) => {
+      console.log("parent ", parent, "context ", context, "info ", info);
       const { id } = args;
       const foundFighter = fightersData.find((fighter) => fighter.id === id);
       if (!foundFighter) {
@@ -61,9 +79,55 @@ const resolvers = {
       return foundFighter;
     },
   },
+
+  Mutation: {
+    addFighter: (
+      parent: any,
+      args: { fighter: FighterType },
+      context: any,
+      info: any,
+    ) => {
+      const { id, name, createdAt } = args.fighter;
+      const newList = [
+        ...fightersData,
+        {
+          id,
+          name,
+          createdAt,
+        },
+      ];
+      return newList;
+    },
+  },
+
+  Date: new GraphQLScalarType({
+    name: "Date",
+    description: "Custom date type",
+    parseValue(value) {
+      // value from the client
+    },
+    serialize(value) {
+      // value sent to the client
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return new Date(ast.value);
+      }
+      return null;
+    },
+  }),
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+// (async () => {
+//   await connectDb();
+// })();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  tracing: true,
+  // context: async () => ({ db: await connectDb() }),
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export enum Status {
+  VERY_AGGRESSIVE = "VERY_AGGRESSIVE",
+  AGGRESSIVE = "AGGRESSIVE",
+  SEMI_AGGRESSIVE = "SEMI_AGGRESSIVE",
+  LIGHT_AGGRESSIVE = "LIGHT_AGGRESSIVE",
+  SMART = "SMART",
+}
+
+export interface Fighter {
+  id: string;
+  name: string;
+  gym?: string;
+  skills?: string[];
+  age?: number;
+  status?: Status;
+  createdAt?: Date;
+}
