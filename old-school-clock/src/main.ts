db6a43d811baef1331601eb6e457d1ef0ad17bc0
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2024-02-24 10:52:13 +0100

    Add clock and buttons to old-school-clock

--- old-school-clock/src/main.ts
+++ old-school-clock/src/main.ts
@@ -1,11 +1,31 @@
 let date = new Date();
-
 let app = document.querySelector<HTMLDivElement>("#app")!;
-app.innerHTML = `<h1>Time is ${date.toDateString()}</h1>`;
+let clock = document.querySelector<HTMLDivElement>("#clock")!;
 
-function updateDate() {
-  date = new Date();
-  app.innerHTML = `<h1>Time is ${date.toLocaleTimeString()}</h1>`;
+function timeIs(date: Date) {
+  return `Time is ${date.toLocaleTimeString()}`;
 }
 
-setInterval(updateDate, 1000);
+clock.innerHTML = `<h1>${timeIs(date)}</h1>`;
+
+function updateDate() {
+  let randomNumber = Math.floor(Math.random() * 100);
+  let color = `rgb(${randomNumber * 2.5}, ${randomNumber + 100}, ${
+    randomNumber + 60
+  })`;
+  clock.innerHTML = `<h1 style="color:${color}"   >${timeIs(new Date())}</h1>`;
+}
+
+let id = setInterval(updateDate, 1000);
+
+let startButton =
+  document.querySelector<HTMLButtonElement>('[data-btn="start"]');
+let stopButton = document.querySelector<HTMLButtonElement>('[data-btn="stop"]');
+
+startButton?.addEventListener("click", () => {
+  id = setInterval(updateDate, 1000);
+});
+
+stopButton?.addEventListener("click", () => {
+  clearInterval(id);
+});
