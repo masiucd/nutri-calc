000ef2669be40f007c56430b3bf47e7b98c83087
masiucd <ciszekmarcell@gmail.com>
2024-03-03 18:24:21 +0100

    Refactor intersection observer logic

--- intersection-observer/src/main.ts
+++ intersection-observer/src/main.ts
@@ -1,19 +1,22 @@
-let box = document.querySelector(".box") as HTMLDivElement;
-
 let observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
-      console.log("🚀 ~ entries.forEach ~ entry:", entry);
+      entry.target.classList.toggle("in-viewport", entry.isIntersecting);
       if (entry.isIntersecting) {
-        console.log("Elemento visible");
-        entry.target.classList.add("visible");
-        entry.target.innerHTML = "<h1>Elemento visible</h1>";
+        observer.unobserve(entry.target); // so we don't observe it again
       }
     });
   },
   {
-    threshold: 1, // 100% where the whole element is visible
+    root: null,
+    // rootMargin: "0px",
+    rootMargin: "0px 0px -100px 0px",
+    threshold: 1, // 0.0 ~ 1.0 (0% ~ 100%)
   }
 );
 
-observer.observe(box);
+let cards = document.querySelectorAll(".card") as NodeListOf<HTMLElement>;
+
+cards.forEach((card) => {
+  observer.observe(card);
+});
