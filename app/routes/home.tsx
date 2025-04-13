05e27ce4357d99a266b05f953bcd060923428d61
masiucd <ciszekmarcell@gmail.com>
2025-04-13 17:48:08 +0200

    feat: implement action function to handle form data and log gender input

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,12 @@
-import {NutritionForm} from "~/components/nutrition-form";
 import type {Route} from "./+types/home";
+
+export async function action({request}: Route.ActionArgs) {
+	let data = await request.formData();
+	let gender = data.get("gender");
+	console.log({gender});
+
+	return null;
+}
 
 export function meta({}: Route.MetaArgs) {
 	return [
@@ -16,9 +23,6 @@ export default function Home() {
 		<>
 			<main className="flex flex-col items-center justify-center pt-16 pb-4">
 				<h1>Welcome to Nutri Calc</h1>
-				<div>
-					<NutritionForm />
-				</div>
 			</main>
 		</>
 	);
