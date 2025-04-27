30b39b66abad4c0250f7aa6c2ba93e98ad148a00
marcell ciszek <ciszekmarcell@gmail.com>
2025-04-27 09:27:09 +0200

    Fluid as a prop, option to make the PageWrapper component fluid on the width.

--- app/components/page-wrapper.tsx
+++ app/components/page-wrapper.tsx
@@ -1,12 +1,24 @@
 import type {PropsWithChildren} from "react";
 import {cn} from "~/lib/utils";
 
+type Props = {
+	className?: string;
+	fluid?: boolean;
+};
+
 export function PageWrapper({
 	children,
 	className,
-}: PropsWithChildren<{className?: string}>) {
+	fluid = false,
+}: PropsWithChildren<Props>) {
 	return (
-		<div className={cn("flex w-full max-w-6xl flex-1 flex-col", className)}>
+		<div
+			className={cn(
+				"flex w-full max-w-6xl flex-1 flex-col",
+				fluid && "max-w-full",
+				className,
+			)}
+		>
 			{children}
 		</div>
 	);
