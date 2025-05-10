c376c6e77e1b23b1837bf37631c0ee9fb4ac1bd2
masiucd <ciszekmarcell@gmail.com>
2025-05-10 10:42:29 +0200

    feat: add Netlify configuration for build and development environments

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,7 +4,7 @@ export default {
 	// Config options...
 	// Server-side render by default, to enable SPA mode set this to `false`
 	ssr: true,
-	async prerender() {
-		return ["/", "/about", "/calculate"];
-	},
+	// async prerender() {
+	// 	return ["/", "/about", "/calculate"];
+	// },
 } satisfies Config;
