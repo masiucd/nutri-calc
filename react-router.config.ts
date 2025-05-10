aac1cb4f76e71c19208dd553206dc4552f90bb4d
masiucd <ciszekmarcell@gmail.com>
2025-05-10 10:45:45 +0200

    refactor: update prerender routes to only include the root path

--- react-router.config.ts
+++ react-router.config.ts
@@ -5,6 +5,6 @@ export default {
 	// Server-side render by default, to enable SPA mode set this to `false`
 	ssr: true,
 	async prerender() {
-		return ["/", "/about", "/calculate"];
+		return ["/"];
 	},
 } satisfies Config;
